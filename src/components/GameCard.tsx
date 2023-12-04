8d57884b33f9e46849568ff92bfef0a530c4c37f
Fahim090 <FAlSami@grantmatch.com>
2023-12-04 11:46:01 -0500

    emoji component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from '../hooks/useGames';
 import PlatformIcons from './PlatformIcon';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-urls';
+import Emoji from './Emoji';
 
 interface Props {
   game: Game;
@@ -22,7 +23,10 @@ export default function GameCard({ game }: Props) {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );

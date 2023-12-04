ed5c7615b23fa068b7f0590f564c58b6022b1a2f
Fahim090 <FAlSami@grantmatch.com>
2023-12-03 21:00:58 -0500

    search input component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ThemeSwitch from './ThemeSwitch';
+import SearchInput from './SearchInput';
 
 export default function Navbar() {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ThemeSwitch />
     </HStack>
   );

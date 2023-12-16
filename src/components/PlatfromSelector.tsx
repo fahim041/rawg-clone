c89de370813dd11084c846cb4fa9aebd9b1934ee
Fahim090 <FAlSami@grantmatch.com>
2023-12-16 14:09:06 -0500

    fetch platforms using react query

--- src/components/PlatfromSelector.tsx
+++ src/components/PlatfromSelector.tsx
@@ -1,24 +1,36 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { BsChevronDown } from "react-icons/bs";
-import usePlatforms from "../hooks/usePlatforms";
-import { Platform } from "../hooks/useGames";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { BsChevronDown } from 'react-icons/bs';
+import usePlatforms from '../hooks/usePlatforms';
+import { Platform } from '../hooks/useGames';
 
 interface Props {
-    onSelectPlatform: (platform: Platform) => void;
-    selectedPlatform: Platform | null;
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-export default function PlatformSelector({onSelectPlatform, selectedPlatform}: Props){
-    const {data, error} = usePlatforms();
+export default function PlatformSelector({
+  onSelectPlatform,
+  selectedPlatform,
+}: Props) {
+  const { data, error } = usePlatforms();
 
-    if(error) return null;
-    return (
-        <Menu>
-            <MenuButton as={Button} rightIcon={<BsChevronDown />}>{selectedPlatform?.name || 'Platforms'}</MenuButton>
-            
-            <MenuList>
-                {data.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
-            </MenuList>
-        </Menu>
-    )
+  if (error) return null;
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatform?.name || 'Platforms'}
+      </MenuButton>
+
+      <MenuList>
+        {data.results.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
 }

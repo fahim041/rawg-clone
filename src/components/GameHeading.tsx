dc5d82c59afc7cdd045c95a517851dbcc483a3c5
Fahim090 <FAlSami@grantmatch.com>
2023-12-16 17:01:13 -0500

    platform hooks and refactor

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,7 +1,7 @@
 import { Heading } from '@chakra-ui/react';
 import { GameQuery } from '../App';
 import useGenres from '../hooks/useGenres';
-import usePlatforms from '../hooks/usePlatforms';
+import usePlatform from '../hooks/usePlatform';
 
 interface Props {
   gameQuery: GameQuery;
@@ -10,10 +10,7 @@ interface Props {
 export default function GameHeading({ gameQuery }: Props) {
   const { data: genres } = useGenres();
   const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
-  const { data: platforms } = usePlatforms();
-  const platform = platforms?.results.find(
-    (p) => p.id === gameQuery.platformId
-  );
+  const platform = usePlatform(gameQuery.platformId);
 
   const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
 

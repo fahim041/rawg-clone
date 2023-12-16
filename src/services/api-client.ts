d42e8229d037325401afa4f77728de2888fa6a5d
Fahim090 <FAlSami@grantmatch.com>
2023-12-16 14:49:22 -0500

    refactor reusable api client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,25 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: '5ca9fd7682794cab838ad08dce7a50c2',
   },
 });
+
+class ApiClient<T> {
+  constructor(public endpoint: string) {}
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default ApiClient;

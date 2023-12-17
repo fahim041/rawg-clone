5022b0977cf26cca5010e124bf865cf9f380856d
Fahim090 <FAlSami@grantmatch.com>
2023-12-17 12:33:45 -0500

    search input improvement

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,29 +1,46 @@
-import { useRef } from 'react';
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { useState } from 'react';
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from '@chakra-ui/react';
 import { BsSearch } from 'react-icons/bs';
+import { MdOutlineClose } from 'react-icons/md';
 
 interface Props {
   onSearch: (searchText: string) => void;
 }
 
 export default function SearchInput({ onSearch }: Props) {
-  const ref = useRef<HTMLInputElement>(null);
+  const [search, setSearch] = useState('');
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        if (ref.current) onSearch(ref.current.value);
+        onSearch(search);
       }}
     >
       <InputGroup>
         <InputLeftElement children={<BsSearch />} />
         <Input
-          ref={ref}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           borderRadius={20}
           placeholder="Search games..."
           variant="filled"
         />
+        {search && (
+          <InputRightElement
+            cursor="pointer"
+            onClick={() => {
+              setSearch('');
+              onSearch('');
+            }}
+            children={<MdOutlineClose />}
+          />
+        )}
       </InputGroup>
     </form>
   );

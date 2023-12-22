5a515342b93c566cec77d408f3623d76e2f1fbb4
Fahim090 <FAlSami@grantmatch.com>
2023-12-21 20:40:36 -0500

    Handle logo click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,18 @@ import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ThemeSwitch from './ThemeSwitch';
 import SearchInput from './SearchInput';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const onClickLogo = () => {
+    navigate('/');
+  };
+
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="60px" cursor="pointer" onClick={onClickLogo} />
       <SearchInput />
       <ThemeSwitch />
     </HStack>

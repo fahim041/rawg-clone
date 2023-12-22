75461c0c116b5cfe8b330ba68de31690ee1731ca
Fahim090 <FAlSami@grantmatch.com>
2023-12-21 21:54:27 -0500

    Navbar component refactor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,14 @@ import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ThemeSwitch from './ThemeSwitch';
 import SearchInput from './SearchInput';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-  const navigate = useNavigate();
-
-  const onClickLogo = () => {
-    navigate('/');
-  };
-
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px" cursor="pointer" onClick={onClickLogo} />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
       <SearchInput />
       <ThemeSwitch />
     </HStack>
